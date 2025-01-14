69622f6e34276c469dcb70ff5345ca378a22dde3
Brownie124 <121064336+Brownie124@users.noreply.github.com>
2025-01-14 17:02:03 -0500

    not done input 01
    almost done

--- src/Input01.java
+++ src/Input01.java
@@ -3,19 +3,21 @@ import javax.swing.*;
 public class Input01 {
     public static void main(String[] args) {
         //Create a JOptionPane.
-        String input = JOptionPane.showInputDialog("lol");
+        String put = JOptionPane.showInputDialog("lol");
         //Store the input as a String and print it.
-        System.out.println();
+        System.out.println(put);
         
         
         //Parse the input as an int.
+        int parse = Integer.parseInt(put);
         //Print its value +1
-        
+        System.out.println("Parse + 1" + parse + 1);
         
         
         //Try creating a dialog, parsing it, and initializing an int in a single line.
+      int num = Integer.parseInt(JOptionPane.showInputDialog("Enter Dialog"));
         //You should have only one semicolon (;) in this line.
-
+         System.out.println(num);
         
     }
 }
