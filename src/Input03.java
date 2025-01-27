bf46ff7aa82ddf92b5d978bc7515fad48aea56c9
Brownie124 <121064336+Brownie124@users.noreply.github.com>
2025-01-27 13:21:33 -0500

    need to fix something
    not done

--- src/Input03.java
+++ src/Input03.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 class Input03 {
 
     public static void main(String[] args) {
         //Create a Scanner
-        
+        Scanner scanner = new Scanner(System.in);
         //Find and print the sum of three integers entered by the user
-        
-        
+       System.out.print("Enter the integers");
+        int u = scanner.nextInt();
+        int a = scanner.nextInt();
+        int r = scanner.nextInt();
+
+        int sum = u + a + r;
+        System.out.println("Sum of the integers"+sum);
         
         
         //Remember to close the Scanner
-        
+        scanner.close();
     }
 }
